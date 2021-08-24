04904fec3e80acf0fc3cb7b90f06de32cc7606e0
yinhuiquan <1258836412@qq.com>
2021-08-24 20:59:45 +0800

    git项目 v1.0

---
+++ src/main/java/cn/baizhi/ProjectGit2Application.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGit2Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/baizhi/ProjectGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

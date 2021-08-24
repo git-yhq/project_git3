b6c55949740664786cf65b689dbfece2abced666
yinhuiquan <1258836412@qq.com>
2021-08-24 21:01:45 +0800

    git项目 v2.0

---
+++ src/test/java/cn/baizhi/tes/TestGitHub.java
@@ -0,0 +1,12 @@
+package cn.baizhi.tes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestGitHub {
+    @Test
+    public void test(){
+        System.out.println("hello github");
+    }
+}
